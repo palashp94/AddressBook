5635f626c521c944a0fb57a866a2afff02a63556
palashp94 <palashmadney@gmail.com>
2022-04-08 18:10:37 +0530

    Added:getter and setter

---
+++ src/CreateContact.java
@@ -0,0 +1,79 @@
+public class CreateContact {
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String city;
+        private int zip;
+        private int phoneNumber;
+        private String email;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public int getZip() {
+            return zip;
+        }
+
+        public void setZip(int zip) {
+            this.zip = zip;
+        }
+
+        public int getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(int phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        @Override
+        public String toString() {
+            return "AddressBook{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", address='" + address + '\'' +
+                    ", city='" + city + '\'' +
+                    ", zip=" + zip +
+                    ", phoneNumber=" + phoneNumber +
+                    ", email='" + email + '\'' +
+                    '}';
+        }
+    }
+
